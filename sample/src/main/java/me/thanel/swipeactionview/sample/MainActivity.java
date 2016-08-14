ee91a47f5228e6d9c9656d848a392014a734ddaf
Tunous <lukus178@gmail.com>
2016-08-14 19:20:08 +0200

    Add sample for toggling swipe directions

--- sample/src/main/java/me/thanel/swipeactionview/sample/MainActivity.java
+++ sample/src/main/java/me/thanel/swipeactionview/sample/MainActivity.java
@@ -7,6 +7,7 @@ import android.widget.Toast;
 import org.jetbrains.annotations.NotNull;
 
 import me.thanel.swipeactionview.SwipeActionView;
+import me.thanel.swipeactionview.SwipeDirection;
 import me.thanel.swipeactionview.SwipeGestureListener;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
         SwipeActionView swipeLeft = (SwipeActionView) findViewById(R.id.swipe_left);
         SwipeActionView swipeRight = (SwipeActionView) findViewById(R.id.swipe_right);
         SwipeActionView swipeBoth = (SwipeActionView) findViewById(R.id.swipe_both);
+        SwipeActionView swipeToggle = (SwipeActionView) findViewById(R.id.swipe_to_toggle);
 
         SwipeGestureListener swipeGestureListener = new SwipeGestureListener() {
             @Override
@@ -37,6 +39,21 @@ public class MainActivity extends AppCompatActivity {
         swipeLeft.setSwipeGestureListener(swipeGestureListener);
         swipeRight.setSwipeGestureListener(swipeGestureListener);
         swipeBoth.setSwipeGestureListener(swipeGestureListener);
+
+        swipeToggle.setSwipeGestureListener(new SwipeGestureListener() {
+            @Override
+            public boolean onSwipedLeft(@NotNull SwipeActionView swipeActionView) {
+                showToast(false);
+                return true;
+            }
+
+            @Override
+            public boolean onSwipedRight(@NotNull SwipeActionView swipeActionView) {
+                boolean enabled = !swipeActionView.hasEnabledDirection(SwipeDirection.Left);
+                swipeActionView.setDirectionEnabled(SwipeDirection.Left, enabled);
+                return true;
+            }
+        });
     }
 
     private void showToast(Boolean swipedRight) {
